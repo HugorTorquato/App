c9a00801da3cd32e922fcd5021100eb1cfa6df0d
Hugo Torquato <hugortorquato@gmail.com>
2025-12-13 12:03:10 -0300

    Renam app_core lib to domain and move main.cp file to server folder

---
+++ backend/src/Server/main.cpp
@@ -0,0 +1,16 @@
+// minimal Crow example (assumes crow headers in vendor/crow/include)
+#include "../../vendor/crow/include/crow.h"  // or #include "crow.h" depending on header you used
+#include "Routes/Utils/General_API_Info_V1.h"
+#include "Utils/Logger.h"
+
+int main() {
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")([]() { return "Hello from Crow backend!"; });
+
+    registerGeneralAPIInfoV1(app);
+
+    Logger::info("Starting Crow server on port 8080");
+    app.port(8080).multithreaded().run();
+    return 0;
+}
